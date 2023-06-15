c98c4bf1f5480215bbd2a81b1452c44c8d3315ee
aimyon <lovepoemlovepoem0516@gmail.com>
2023-06-15 22:41:48 +0900

    @Qualifier("ExceptionHandlerController") 빈충돌 핸들러 수정

--- reservation/src/main/java/com/itwillbs/test/handler/ExceptionHandlerController.java
+++ reservation/src/main/java/com/itwillbs/test/handler/ExceptionHandlerController.java
@@ -1,13 +1,15 @@
 package com.itwillbs.test.handler;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jdbc.BadSqlGrammarException;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
+@Qualifier("ExceptionHandlerController")
 public class ExceptionHandlerController {
-
+	
     @ExceptionHandler(BadSqlGrammarException.class)
     public String handleBadSqlGrammarException(BadSqlGrammarException ex, Model model) {
         String msg = "해당 테이블을 찾을 수 없습니다. 테이블을 생성해주세요";
