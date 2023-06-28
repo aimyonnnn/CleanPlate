40e88e92caf06169bf2665ef4c9037dbe2518874
user <user@ITWILL>
2023-06-28 14:27:07 +0900

    StoredashBoard ceotest에 대해서 값을 뜨게 설정했습니다!

--- reservation/src/main/java/com/itwillbs/test/mapper/StoredashMapper.java
+++ reservation/src/main/java/com/itwillbs/test/mapper/StoredashMapper.java
@@ -1,50 +1,25 @@
 package com.itwillbs.test.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Map;
-
 @Mapper
-@Repository
 public interface StoredashMapper {
-    @Select("SELECT COUNT(*) AS todayReservationCount "
-    	    +"FROM payment "
-    	    +"WHERE DATE(p_orderDate) = CURDATE()")
-    List<Integer> getTodayReservationCounts();
-
-    @Select("SELECT COUNT(*) AS weeklyReservationCount "
-    		+"FROM payment "
-    		+"WHERE DATE(p_orderDate) "
-    		+"BETWEEN DATE_SUB(CURDATE(), INTERVAL 6 DAY) AND CURDATE()")
-    List<Integer> getMonthlyReservationCounts();
+    // 오늘의 총 예약수
+    int getTodayReservationCounts(String cId);
     
-  //일일 예약수
-    @Select("SELECT COUNT(*) "
-    		+ "FROM payment WHERE DATE(p_orderDate) "
-    		+ "BETWEEN DATE_SUB(CURDATE(), INTERVAL 6 DAY) AND CURDATE() "
-    		+ "GROUP BY DATE(p_orderDate)")
-    List<Integer> getReservationCountsByWeek();
+    // 이번 달 총 예약수
+    int getMonthlyReservationCounts(String cId);
+    
+    // 일간 매출 조회
+    int getTodaySalesAmount(String cId);
+    
+    // 일주일 매출 조회
+    int getWeeklyallSalesAmount(String cId);
+    
+    //일간 예약 수 조회
+    List<Integer> getReservationCount(String cId);
     
     // 최근 1주일 간 매출 금액 조회
-    @Select("SELECT SUM(payment_total_price) "
-    		+ "FROM payment WHERE p_orderDate "
-    		+ "BETWEEN DATE_SUB(CURDATE(), INTERVAL 6 DAY) AND CURDATE() "
-    		+ "GROUP BY DAY(p_orderDate)")
-    List<Integer> getWeeklySalesAmount();
-
-    // 일일 매출액
-    @Select("SELECT SUM(payment_total_price) "
-            + "FROM payment "
-            + "WHERE DATE(p_orderDate) = CURDATE()")
-    List<Integer> getTodaySalesAmount();
-
-    //일주일간 매출액
-    @Select("SELECT SUM(payment_total_price) "
-            + "FROM payment "
-            + "WHERE DATE(p_orderDate) BETWEEN DATE_SUB(CURDATE(), INTERVAL 6 DAY) AND CURDATE()")
-	List<Integer> getWeeklyallSalesAmount();
-
+    List<Integer> getWeeklySalesAmount(String cId);
 }
--- reservation/src/main/java/com/itwillbs/test/vo/StoredashVO.java
+++ reservation/src/main/java/com/itwillbs/test/vo/StoredashVO.java
@@ -1,26 +1,13 @@
 package com.itwillbs.test.vo;
 
+import lombok.Data;
+
+@Data
 public class StoredashVO {
 	private int storeId;
 	private int reservationCount;
 	private String c_id;
-	public int getStoreId() {
-		return storeId;
-	}
-	public void setStoreId(int storeId) {
-		this.storeId = storeId;
-	}
-	public int getReservationCount() {
-		return reservationCount;
-	}
-	public void setReservationCount(int reservationCount) {
-		this.reservationCount = reservationCount;
-	}
-	public String getC_id() {
-		return c_id;
-	}
-	public void setC_id(String c_id) {
-		this.c_id = c_id;
-	}
-
+	private String orderDate;
+	private int salesAmount;
+	private int salesCount;
 }
